6929ab6941268a57e5a82e9f6f49e4785cc5b9d8
Bhanu Pachchilla <69502123+Nanimani273@users.noreply.github.com>
2021-07-26 21:40:35 +0530

    Add files via upload

---
+++ UPSTAC-api-stub/src/test/java/org/upgrad/upstac/testrequests/TestRequestServiceTest.java
@@ -0,0 +1,11 @@
+package org.upgrad.upstac.testrequests;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TestRequestServiceTest {
+
+}
